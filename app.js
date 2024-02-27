efeb7866289bddb149a8844412e7e1cc7f70ba6d
FellipeCastro <fellipecastro27d@gmail.com>
2024-02-27 19:06:57 -0300

    cadastro de usuários funcionando

--- app.js
+++ app.js
@@ -9,9 +9,60 @@ const app = express()
 
 const PORT = 3000
 
+// Config JSON response
+app.use(express.json())
+
+// Models
+const User = require('./models/User.js')
+
 // Open Route - Public Route
 app.get('/', (req, res) => {
     res.status(200).json({ msg: 'Hello World' })
+})
+
+// Register User
+app.post('/auth/register', async (req, res) => {
+    const {name, email, password, confirmPassword} = req.body
+
+    // Validações
+    if (!name) {
+        return res.status(422).json({ msg: 'O nome é obrigatório!' })
+    }
+    if (!email) {
+        return res.status(422).json({ msg: 'O e-mail é obrigatório!' })
+    }
+    if (!password) {
+        return res.status(422).json({ msg: 'A senha é obrigatório!' })
+    }
+    if (password !== confirmPassword) {
+        return res.status(422).json({ msg: 'As senhas devem ser iguais!' })
+    }
+
+    // Check if user exists
+    const userExists = await User.findOne({ email: email })
+
+    if (userExists) {
+        return res.status(422).json({ msg: 'Usuário já cadastrado!' })
+    }
+
+    // Create password
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // Create user
+    const user = new User({
+        name,
+        email,
+        password,
+    })
+
+    try {
+        await user.save()
+        res.status(201).json({ msg: 'Usuário cadastrado com sucesso!' })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ msg: 'Erro interno do servidor, tente novamente mais tarde!' })
+    }
 })
 
 // Credencials
